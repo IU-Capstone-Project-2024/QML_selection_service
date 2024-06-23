d93a9249eead3640ee03bd80e6832845aa5ce855
VechkanovVV <hatavidit84@gmail.com>
2024-06-23 21:59:11 +0300

    code refactoring

--- front/lib/app/view/app.dart
+++ front/lib/app/view/app.dart
@@ -26,7 +26,7 @@ class App extends StatelessWidget {
         },
         child: MaterialApp.router(
           routerConfig: AppRouter.router,
-          title: 'Flutter Demo',
+          title: 'QML Selection Service',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
